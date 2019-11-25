b254a0c92f8d8ff2420ce3c4b7efe547c38b29c6
melia miller <meliajm@gmail.com>
2019-11-25 01:25:29 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,6 +41,6 @@ def naming_system
   #  Species
   
   PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
-  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+  PORTION_2[:sub_category] = PORTION_3
   PORTION_1
 end
